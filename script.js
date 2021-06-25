6ed929d2269a1fb0b34b8589b3ff757e7e0e6f0d
Annika Heiling <aheiling@uiowa.edu>
2021-06-25 02:51:57 -0500

    hacky, but works

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const canvas = document.querySelector("#sketchContainer");
-
-//eventually, let players select grid size: that would go here for '16'
-for (let i=256; i>0; i--) {
-    const square = document.createElement('div');
-    square.textContent = "square"; //debug
-    square.setAttribute("id", "sq"+i);
-    square.setAttribute("class", "sq");
-    canvas.appendChild(square);
+const userDims = 16;
+/*
+function gridGenerator(userDims) {
+    for (let i=userDims*userDims; i>0; i--) {
+        const square = document.createElement('div');
+        //square.textContent = "square"; //debug
+        square.setAttribute("id", "sq"+i);
+        square.setAttribute("class", "sq");
+        canvas.appendChild(square);
+    }
+}*/
+function gridGenerator(userDims) {
+    for (let i=userDims; i>0; i--) {
+        //make userDims rows
+        const row = document.createElement('div');
+        row.setAttribute("id", "row"+i); //potentially unnecessary...
+        row.setAttribute("class", "row");
+        canvas.appendChild(row);
+        for  (let j=userDims; j>0; j--) { 
+        //make userDims squares per row
+            const square = document.createElement('div');
+            //square.textContent = "square"; //debug
+            square.setAttribute("id", i+"sq"+j);
+            square.setAttribute("class", "sq");
+            row.appendChild(square);
+        }
+    }
+    window.getComputedStyle(document.documentElement).getPropertyValue('--colNum'); ///
+    document.documentElement.style.setProperty("--colNum", userDims);
+    //window.getComputedStyle(document.documentElement).getPropertyValue('--sqSize'); ///
+    //document.documentElement.style.setProperty("--sqSize",);
 }
+gridGenerator(userDims);
 
-const squares = document.querySelectorAll('.sq');
+let squares = document.querySelectorAll('.sq');
 /*console.log(squares);*/
 function addClassNodeList(list, event) {
     for (let i=0, len = list.length; i < len; i++) {
@@ -22,12 +46,16 @@ addClassNodeList(squares, "mouseenter");
 
 /*Everything below this line needs to be reworked, maybe entirely, to get the 
 reset button to work.*/
-function removeClassNodeList(list, event) {
-    for (let i=0, len = list.length; i < len; i++) {
-            document.getElementById(list[i].id).addEventListener(event, function() {
-            list[i].remove("class","colored");
-        });
-    }
+function removeClassNodeList() {
+    /*for (let i=0, len = squares.length; i < len; i++) {
+            //document.getElementById(list[i].id).addEventListener(event, function() {
+            squares[i].remove("class","colored");
+        //});
+    }*/
+    canvas.innerHTML = "";
+    gridGenerator(prompt("How many squares tall/wide do you want your Etch-a-Sketch to be?", "input a number 1-100"));
+    squares = document.querySelectorAll('.sq');
+    addClassNodeList(squares, "mouseenter");
 }
-document.getElementById("restart").addEventListener("click", removeClassNodeList(squares, "click"));
+//document.getElementById("restart").addEventListener("click", removeClassNodeList(squares, "click"));
 //squares.addEventListener("hover", squares.setAttribute("class", "colored"));
